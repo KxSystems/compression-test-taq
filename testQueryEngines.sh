e9ddbd3760f560fc78f5559f502daefe2bf7dd60
Ferenc Bodon <fbodon@kx.com>
2026-03-12 23:31:25 -0700

    KDBX-124 restrucutre

--- testQueryEngines.sh
+++ testQueryEngines.sh
@@ -61,14 +61,14 @@ function execute_queries () {
     local COMMONPARAMS="-querymeta ./artifacts/queries/querymeta.psv -paramdir ${PARAM_DIR}"
     for s in "${THREAD_NRS[@]}"; do
         echo "--> Running with $s threads"
-        $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/kdb -format kdb -queryfile ./artifacts/queries/kdb.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdb_kdb_${s}Threads.psv -s ${s}
-        $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/kdb -format kdb -queryfile ./artifacts/queries/sql.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdbsql_kdb_${s}Threads.psv -engine sql -s ${s}
-        QMAP=TRUE $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/kdb -format kdb -queryfile ./artifacts/queries/kdb_peach.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdbPeachQMAP_kdb_${s}Threads.psv -s ${s}
-        $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/parquet/rowgroup -format parquet_rowgroup -queryfile ./artifacts/queries/parquet_rowgroup.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdb_parquetRowgroup_${s}Threads.psv -s ${s}
+        $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/kdb -format kdb -queryfile ./artifacts/queries/ondisk/kdb.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdb_kdb_${s}Threads.psv -s ${s}
+        $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/kdb -format kdb -queryfile ./artifacts/queries/ondisk/sql.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdbsql_kdb_${s}Threads.psv -engine sql -s ${s}
+        QMAP=TRUE $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/kdb -format kdb -queryfile ./artifacts/queries/ondisk/kdb_peach.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdbPeachQMAP_kdb_${s}Threads.psv -s ${s}
+        $(get_numa_config) $QEXEC ./src/runQueries.q -db ${DB_DIR}/parquet/rowgroup -format parquet_rowgroup -queryfile ./artifacts/queries/ondisk/parquet_rowgroup.psv ${COMMONPARAMS} -result ${RESULT_DIR}/kdb_parquetRowgroup_${s}Threads.psv -s ${s}
 
-        POLARS_MAX_THREADS=$(( s > 1 ? s : 1 )) $(get_numa_config) python3 pysrc/run_queries.py -engine polars -db ${DB_DIR}/parquet/rowgroup -queryfile ./artifacts/queries/polars.psv ${COMMONPARAMS} -result ${RESULT_DIR}/polars_parquetRowgroup_${s}Threads.psv
-        QARGS="-s ${s}" $(get_numa_config) python3 pysrc/run_queries.py -engine pykx -db ${DB_DIR}/kdb -queryfile ./artifacts/queries/pykx.psv ${COMMONPARAMS} -result ${RESULT_DIR}/pykx_kdb_${s}Threads.psv
-        QARGS="-s ${s}" $(get_numa_config) python3 pysrc/run_queries.py -engine pykxq -db ${DB_DIR}/kdb -queryfile ./artifacts/queries/kdb.psv ${COMMONPARAMS} -result ${RESULT_DIR}/pykxq_kdb_${s}Threads.psv
+        POLARS_MAX_THREADS=$(( s > 1 ? s : 1 )) $(get_numa_config) python3 pysrc/run_queries.py -engine polars -db ${DB_DIR}/parquet/rowgroup -queryfile ./artifacts/queries/ondisk/polars.psv ${COMMONPARAMS} -result ${RESULT_DIR}/polars_parquetRowgroup_${s}Threads.psv
+        QARGS="-s ${s}" $(get_numa_config) python3 pysrc/run_queries.py -engine pykx -db ${DB_DIR}/kdb -queryfile ./artifacts/queries/ondisk/pykx.psv ${COMMONPARAMS} -result ${RESULT_DIR}/pykx_kdb_${s}Threads.psv
+        QARGS="-s ${s}" $(get_numa_config) python3 pysrc/run_queries.py -engine pykxq -db ${DB_DIR}/kdb -queryfile ./artifacts/queries/ondisk/kdb.psv ${COMMONPARAMS} -result ${RESULT_DIR}/pykxq_kdb_${s}Threads.psv
     done
 }
 
